bceb2c28be03656b2c49574aaad887918311e3e0
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-10 23:10:45 -0500

    Add concat.h

---
+++ include/preproc/concat.h
@@ -0,0 +1,9 @@
+#ifndef C_PREPROCESSOR_CONCAT_H
+#define C_PREPROCESSOR_CONCAT_H
+
+// Local Includes
+#include "prv/concat_prv.h"
+
+#define c_preproc_concat(a, b) _c_preproc_concat(a, b)
+
+#endif // #ifndef C_PREPROCESSOR_CONCAT_H
