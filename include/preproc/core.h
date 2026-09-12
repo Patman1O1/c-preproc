dab6c6485ed0c98e757eff140127edd0883f9390
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-12 13:32:42 -0500

    Implement expand, concat, and strify

--- include/preproc/core.h
+++ include/preproc/core.h
@@ -1,6 +1,13 @@
 #ifndef C_PREPROCESSOR_CORE_H
 #define C_PREPROCESSOR_CORE_H
 
+// Local Includes
+#include "prv/core_prv.h"
+
 #define c_preproc_expand(...) __VA_ARGS__
+
+#define c_preproc_concat(x, y) _c_preproc_concat(x, y)
+
+#define c_preproc_strify(x) _c_preproc_strify(x)
 
 #endif // #ifndef C_PREPROCESSOR_CORE_H
