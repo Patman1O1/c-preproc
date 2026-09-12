402144adac6b796681833495a5b12e18eb5716e9
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-12 13:34:14 -0500

    Add core_prv.h

---
+++ include/preproc/prv/core_prv.h
@@ -0,0 +1,8 @@
+#ifndef C_PREPROCESSOR_CORE_PRIVATE_H
+#define C_PREPROCESSOR_CORE_PRIVATE_H
+
+#define _c_preproc_concat(x, y) x##y
+
+#define _c_preproc_strify(x) #x
+
+#endif // #ifndef C_PREPROCESSOR_CORE_PRIVATE_H
