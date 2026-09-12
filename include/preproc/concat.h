0e0920a2f343e05223534d62fa184ec8ea079f4b
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-12 13:33:28 -0500

    Remove concat.h

--- include/preproc/concat.h
+++
@@ -1,9 +0,0 @@
-#ifndef C_PREPROCESSOR_CONCAT_H
-#define C_PREPROCESSOR_CONCAT_H
-
-// Local Includes
-#include "prv/concat_prv.h"
-
-#define c_preproc_concat(a, b) _c_preproc_concat(a, b)
-
-#endif // #ifndef C_PREPROCESSOR_CONCAT_H
